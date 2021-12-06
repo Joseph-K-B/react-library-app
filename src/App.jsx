c6e0d55dbd9cd104a9ad8fd8a502fcb04c450735
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-12-06 13:53:13 -0800

    random extras

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
+import Home from './views/Home'
 import './App.css'
+import BluePill from './views/BluePill'
 
 function App() {
   // TODO: Add routes to books & views
@@ -10,19 +12,22 @@ function App() {
       <Router>
         <header>
           <h1>Library Catalog</h1>
-          <NavLink to="/" exact>
-            Home
-          </NavLink>
-          <NavLink to="/books" exact>
-            Library
-          </NavLink>
+          <nav>
+            <NavLink className="homeNav" to="/" exact>
+              Home
+            </NavLink>
+            <NavLink className="catalogNav" to="/books" exact>
+              Library
+            </NavLink>
+          </nav>
         </header>
         <main className="container">
           <Switch>
+            <Route exact path="/" component={Home} />
             <Route exact path="/books" component={BookList} />
             <Route exact path="/books/:id" component={BookDetail} />
+            <Route exact path="/blue" component={BluePill} />
           </Switch>
-          {/* <Route exact path="/" component={Home} /> */}
         </main>
         <footer>
           <p>Footer</p>
