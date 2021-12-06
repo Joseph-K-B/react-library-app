59b03d0d829c3ab23e5103b08667e3f377ede4be
Joseph Brown <84300225+Joseph-K-B@users.noreply.github.com>
2021-12-06 11:37:00 -0800

    Routes and NavLinks working

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, NavLink, Route, Switch } from 'react-router-dom'
 import BookList from './views/Books/BookList'
 import BookDetail from './views/Books/BookDetail'
 import './App.css'
@@ -5,10 +6,29 @@ import './App.css'
 function App() {
   // TODO: Add routes to books & views
   return (
-    <main className="container">
-      <h1>Library Catalog</h1>
-      <BookList />
-    </main>
+    <>
+      <header>
+        <h1>Library Catalog</h1>
+        <Router>
+          <NavLink to="/" exact>
+            Home
+          </NavLink>
+          <NavLink to="/books">BookList</NavLink>
+        </Router>
+      </header>
+      <main className="container">
+        <Router>
+          <Switch>
+            <Route exact path="/books" component={BookList} />
+            <Route exact path="/books/:id" component={BookDetail} />
+          </Switch>
+        </Router>
+        {/* <Route exact path="/" component={Home} /> */}
+      </main>
+      <footer>
+        <p>Footer</p>
+      </footer>
+    </>
   )
 }
 
